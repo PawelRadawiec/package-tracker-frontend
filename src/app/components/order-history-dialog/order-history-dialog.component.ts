df4360a34d6040b1c58eaff5cdce4d0c1f7f1926
PawelRadawiec <pawel.radaw@gmail.com>
2020-09-08 22:24:29 +0200

    history dialog / set order code

--- src/app/components/order-history-dialog/order-history-dialog.component.ts
+++ src/app/components/order-history-dialog/order-history-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { OrderHistory } from 'src/app/models/order.model';
 import { Subscription } from 'rxjs';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-order-history-dialog',
@@ -14,11 +15,25 @@ export class OrderHistoryDialogComponent implements OnInit {
   orderCode: string;
   subscribe: Subscription;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialog: MatDialogRef<OrderHistoryDialogComponent>
+  ) {
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.orderHistory = this.data.orderHistory;
+    this.setOrderCode();
+  }
+
+  setOrderCode() {
+    if (Array.isArray(this.orderHistory) && this.orderHistory.length > 0) {
+      this.orderCode = _.get(this.orderHistory, [0, 'code'], '');
+    }
+  }
+
+  close() {
+    this.dialog.close();
   }
 
 }
